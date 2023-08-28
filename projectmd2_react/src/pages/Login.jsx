5fedec977b8415efe51d4f634a10e050a4109603
Phạm Trần Việt Anh <56873294+vanhitsup@users.noreply.github.com>
2023-08-29 00:20:51 +0700

    up

--- projectmd2_react/src/pages/Login.jsx
+++ projectmd2_react/src/pages/Login.jsx
@@ -1,19 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Login.css";
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
+
+// CommonJS
 function Login() {
+  const [userData, setUserData] = useState([]);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/users")
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  console.log(userData);
+  console.log(loginData);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = userData.find(
+      (user) =>
+        user.email === loginData.email &&
+        user.password === loginData.password
+    );
+    console.log(user);
+    if (user) {
+      // Login successful, you can perform further actions here
+      axios
+        .patch(`http://localhost:8000/users/${user.id}`, {
+          isLogin: true,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      localStorage.setItem("isLoginId", user.id);
+      Swal.fire({
+        title: 'success!',
+        text: 'Logged in successfully!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      }) 
+      navigate("/");
+    } else {
+      // Invalid credentials
+
+      Swal.fire({
+        title: 'Error!',
+        text: 'Login failed!',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })    }
+  };
   return (
     <div>
       <div className="container-main">
         <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
   {/* Email input */}
   <div className="form-outline mb-4">
-    <input type="email" id="form2Example1" className="form-control" />
+    <input type="email" id="form2Example1" name="email" className="form-control" onChange={handleInputChange} value={loginData.email} />
     <label className="form-label" htmlFor="form2Example1">Email address</label>
   </div>
   {/* Password input */}
   <div className="form-outline mb-4">
-    <input type="password" id="form2Example2" className="form-control" />
+    <input type="password" name="password" id="form2Example2" className="form-control" onChange={handleInputChange} value={loginData.password}  />
     <label className="form-label" htmlFor="form2Example2">Password</label>
   </div>
   {/* 2 column grid layout for inline styling */}
@@ -31,7 +100,7 @@ function Login() {
     </div>
   </div>
   {/* Submit button */}
-  <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
+  <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
   {/* Register buttons */}
   <div className="text-center">
     <p>Not a member? <a href="#!">Register</a></p>
@@ -45,7 +114,7 @@ function Login() {
     <button type="button" className="btn btn-link btn-floating mx-1">
       <i className="fab fa-twitter" />
     </button>
-    <button type="button" className="btn btn-link btn-floating mx-1">
+    <button type="submit" className="btn btn-link btn-floating mx-1">
       <i className="fab fa-github" />
     </button>
   </div>
